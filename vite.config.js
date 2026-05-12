7caefac46c398a8d2b3170ae5b27120910e01176
AnotherLevelDecants <anotherleveldecants@gmail.com>
2026-05-12 12:20:37 +0200

    Auto-copy data files in build process

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite'
+import { copyFileSync, mkdirSync } from 'fs'
+import { join } from 'path'
 
 export default defineConfig({
   server: {
@@ -8,5 +10,19 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     minify: 'terser'
-  }
+  },
+  plugins: [
+    {
+      name: 'copy-data',
+      apply: 'build',
+      writeBundle() {
+        const srcDir = join(process.cwd(), 'data')
+        const destDir = join(process.cwd(), 'dist', 'data')
+        
+        mkdirSync(destDir, { recursive: true })
+        copyFileSync(join(srcDir, 'perfumes.json'), join(destDir, 'perfumes.json'))
+        copyFileSync(join(srcDir, 'packs.json'), join(destDir, 'packs.json'))
+      }
+    }
+  ]
 })
